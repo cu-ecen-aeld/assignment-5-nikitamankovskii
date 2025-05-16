5492bdca449150f43360d90c847f84bd76f6bf26
vboxuser <vboxuser@Ubuntu20.04.myguest.virtualbox.org>
2025-05-16 14:40:49 +0400

    addded clean.sh, added openssh pckg

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+AESD_ASSIGNMENTS_VERSION = 'b3049b78ee7e077e6821253e5342b4d1a142d47a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
